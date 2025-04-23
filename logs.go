c828642aadf1d3f77a421e0fc1103ee1a297a2ae
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2025-04-23 15:44:40 +0200

    feat: add logs

---
+++ logs.go
@@ -0,0 +1,80 @@
+package core
+
+import (
+	"os"
+
+	"github.com/gookit/color"
+)
+
+type LogLevel string
+
+const (
+	InfoLevel  LogLevel = "info"
+	ErrorLevel LogLevel = "error"
+	DebugLevel LogLevel = "debug"
+)
+
+func Init() error {
+	root, err := FindOptiqueJson()
+	if err != nil {
+		return err
+	}
+	manifest, err := ReadProjectManifestAt(root + "/" + PROJECT_MANIFEST)
+	os.Setenv("OPTIQUE_SERVICE", manifest.Name)
+	return nil
+}
+
+func Info(msg string) {
+	Log(&LogOptions{
+		Level:   InfoLevel,
+		Service: RetrieveServiceName(),
+		Message: msg,
+	})
+}
+
+func RetrieveServiceName() string {
+	service, ok := os.LookupEnv("OPTIQUE_SERVICE")
+	if !ok {
+		service = "optique"
+	}
+	return service
+}
+
+func Error(msg string) {
+	Log(&LogOptions{
+		Level:   ErrorLevel,
+		Service: RetrieveServiceName(),
+		Message: msg,
+	})
+}
+
+func Debug(msg string) {
+	Log(&LogOptions{
+		Level:   DebugLevel,
+		Service: RetrieveServiceName(),
+		Message: msg,
+	})
+}
+
+type LogOptions struct {
+	Level   LogLevel
+	Service string
+	Message string
+}
+
+func Log(options *LogOptions) {
+	switch options.Level {
+	case InfoLevel:
+		color.New(color.FgWhite, color.BgCyan).Print("INFO")
+		color.New(color.FgCyan).Print(options.Service)
+		color.New(color.FgGray.Light()).Print(options.Service)
+	case ErrorLevel:
+		color.New(color.FgWhite, color.BgRed).Print("ERROR")
+		color.New(color.FgRed).Print(options.Service)
+		color.New(color.FgGray.Light()).Print(options.Service)
+	case DebugLevel:
+		color.New(color.FgWhite, color.BgBlue).Print("DEBUG")
+		color.New(color.FgBlue).Print(options.Service)
+		color.New(color.FgGray.Light()).Print(options.Service)
+	}
+}
